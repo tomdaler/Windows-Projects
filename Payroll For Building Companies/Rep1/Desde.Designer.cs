898a2e829736bde56349a13a645443c91f319fe0
Tomas Dale <101162223+tomdaler@users.noreply.github.com>
2022-04-11 12:52:34 -0600

    Add files via upload

---
+++ Payroll For Building Companies/Rep1/Desde.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Rep1
+{
+    partial class Desde
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.N1 = new System.Windows.Forms.NumericUpDown();
+            this.cbMes = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.N1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // N1
+            // 
+            this.N1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N1.Location = new System.Drawing.Point(81, 28);
+            this.N1.Name = "N1";
+            this.N1.Size = new System.Drawing.Size(62, 24);
+            this.N1.TabIndex = 166;
+            // 
+            // cbMes
+            // 
+            this.cbMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMes.FormattingEnabled = true;
+            this.cbMes.Location = new System.Drawing.Point(81, 58);
+            this.cbMes.Name = "cbMes";
+            this.cbMes.Size = new System.Drawing.Size(121, 26);
+            this.cbMes.TabIndex = 165;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 18);
+            this.label2.TabIndex = 164;
+            this.label2.Text = "Mes";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 18);
+            this.label1.TabIndex = 163;
+            this.label1.Text = "Año";
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Controls.Add(this.N1);
+            this.Controls.Add(this.cbMes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(215, 105);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.N1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown N1;
+        private System.Windows.Forms.ComboBox cbMes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Payroll For Building Companies/Rep1/Desde.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Rep1
+{
+    public partial class Desde: UserControl
+    {
+        public Desde()
+        {
+            InitializeComponent();
+        }
+        
+        public string anio()
+        {
+            return N1.Value.ToString();
+        }
+        public string mes()
+        {
+            int mes = cbMes.SelectedIndex + 1;
+            return mes.ToString();
+        }
+        public string Fec1()
+        {
+            int mes = cbMes.SelectedIndex + 1;
+            int ani = Convert.ToInt32(N1.Value.ToString());
+            int fec1 = ani * 100 + mes;
+            return fec1.ToString();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            cbMes.Items.Insert(0, "Enero");
+            cbMes.Items.Insert(1, "Febrero");
+            cbMes.Items.Insert(2, "Marzo");
+            cbMes.Items.Insert(3, "Abril");
+            cbMes.Items.Insert(4, "Mayo");
+            cbMes.Items.Insert(5, "Junio");
+            cbMes.Items.Insert(6, "Julio");
+            cbMes.Items.Insert(7, "Agosto");
+            cbMes.Items.Insert(8, "Septiembre");
+            cbMes.Items.Insert(9, "Octubre");
+            cbMes.Items.Insert(10, "Noviembre");
+            cbMes.Items.Insert(11, "Diciembre");
+                       
+            cbMes.SelectedIndex = 0;
+           
+            N1.Maximum = DateTime.Now.Year;
+            N1.Value = DateTime.Now.Year;
+            N1.Minimum = DateTime.Now.Year - 10;
+        }
+    }
+}
